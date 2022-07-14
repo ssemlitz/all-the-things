230966c77ba75594a2042cd203215ec2ea57bea6
ssemlitz <sdsemlitz@yahoo.com>
2022-07-14 13:59:46 -0400

    add navlink to alpha things

--- src/App.js
+++ src/App.js
@@ -133,7 +133,7 @@ const App = () => {
     {
       name: "absolute unit",
       image: "https://imgs.xkcd.com/comics/beautiful.png",  
-      attributes: ["size", "width", "sex symbol", "beautiful"],
+      attributes: ["size", "width", "sex symbol", "beautiful", "the hottest thing around"],
     },
   ])
 
